48df536c93fb06211b1ab6f88f442d12f063297c
oleksandrmoskalenko <moskalenko.developer@gmail.com>
2023-05-02 16:53:09 +0300

    finished review section

---
+++ js/review-slider.js
@@ -0,0 +1,16 @@
+const swiper2 = new Swiper('.review__slider', {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween : 0,
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
