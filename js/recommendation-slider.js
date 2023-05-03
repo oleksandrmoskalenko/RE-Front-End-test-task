1ca2d80a93e96dcdfc9ee5f0f4adc26e990718d9
oleksandrmoskalenko <moskalenko.developer@gmail.com>
2023-05-03 20:22:04 +0300

    fixed slider in recommendation section

--- js/recommendation-slider.js
+++ js/recommendation-slider.js
@@ -1,11 +1,11 @@
 const recommendationSwiper = new Swiper('.recommendation__slider', {
   direction: 'horizontal',
   slidesPerView: 'auto',
+  grabCursor: true,
   spaceBetween: 0,
-  // centeredSlides: true,
   loop: true,
 
   autoplay: {
-    delay: 5000,
+    delay: 3000,
   },
 });
