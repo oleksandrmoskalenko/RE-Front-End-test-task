70e2582789904b63e2cc7caa5bdee068d95025db
oleksandrmoskalenko <moskalenko.developer@gmail.com>
2023-05-04 01:48:42 +0300

    added filter to the recommendation slider; the slides are filtered by 'house', 'villa' or 'apartment' filter-marker

---
+++ js/sliders/recommendation-slider.js
@@ -0,0 +1,45 @@
+const recommendationSwiper = new Swiper('.recommendation__slider', {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  grabCursor: true,
+  spaceBetween: 0,
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  navigation: {
+    nextEl: '.recommendation__button-next',
+    prevEl: '.recommendation__button-prev',
+  },
+});
+
+function filterSlides(button) {
+  return button.on("click", function(){
+    const filter = $(this).html().toLowerCase();
+  
+    $(".swiper-slide").not("[data-filter='"+filter+"']").addClass("non-swiper-slide").removeClass("swiper-slide").hide();
+      $("[data-filter='"+filter+"']").removeClass("non-swiper-slide").addClass("swiper-slide").attr("style", null).show();
+      recommendationSwiper.update();
+      recommendationSwiper.destroy();
+      recommendationSwiper = new Swiper('.recommendation__slider', {
+        direction: 'horizontal',
+        slidesPerView: 'auto',
+        grabCursor: true,
+        spaceBetween: 0,
+        loop: true,
+  
+        autoplay: {
+          delay: 3000,
+        },
+      
+        navigation: {
+          nextEl: '.recommendation__button-next',
+          prevEl: '.recommendation__button-prev',
+        },
+    });
+  });
+}
+
+filterSlides($(".recommendation__buttons-wrapper button"));
